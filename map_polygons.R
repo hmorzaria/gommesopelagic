779101c63534a952330ece9e8ccc41929ac18612
hmorzaria <hmorzaria@hotmail.com>
2019-08-20 16:55:27 -0700

    Corrected food web diagram

--- map_polygons.R
+++ map_polygons.R
@@ -5,34 +5,45 @@
 #' @details Used code from https://www.r-spatial.org/r/2018/10/25/ggplot2-sf.html
 #' @author Hem Nalini Morzaria-Luna, hmorzarialuna@gmail.com
 
+# set locale to avoid multibyte errors
+Sys.setlocale("LC_CTYPE", "en_US.UTF-8")
+# https://www.r-bloggers.com/web-scraping-and-invalid-multibyte-string/
 
+#' LIBRARIES
+#' -----------------
+# List of packages for session
+.packages = c("rgdal","data.table","tidyverse","here","maptools","broom","ggmap",
+              "sf", "rnaturalearth", "rnaturalearthdata","ggspatial","rgeos")
 
-make_map <- function(shape.file, file.name,scale.factor, bar.position) {
-  
+# Install CRAN packages (if not already installed)
+.inst <- .packages %in% installed.packages()
+if(length(.packages[!.inst]) > 0) install.packages(.packages[!.inst])
+
+# Load packages into session 
+lapply(.packages, require, character.only=TRUE)
+
+make_map <- function(shape.file, file.name) {
 
   model.shape <- readOGR(shape.file)
   
   # Reformat shape for mapping purposes
   model.shape.df <- broom::tidy(model.shape)
   
-  #tm_shape(world) + tm_polygons()
-  #tm_basemap(server = "OpenTopoMap")
-  
-  world <- ne_countries(scale = "large", returnclass = "sf")
+  world <- ne_countries(scale = "medium", returnclass = "sf")
   class(world)
   
   world_points<- st_centroid(world)
   world_points <- cbind(world, st_coordinates(st_centroid(world$geometry)))
   
-  min.long <- min(model.shape.df$long)-scale.factor
-  max.long <- max(model.shape.df$long)+scale.factor
-  min.lat <- min(model.shape.df$lat)-scale.factor
-  max.lat <- max(model.shape.df$lat)+scale.factor
+  min.long <- min(model.shape.df$long)-3
+  max.long <- max(model.shape.df$long)+2
+  min.lat <- min(model.shape.df$lat)-2
+  max.lat <- max(model.shape.df$lat)+3
   
   
   model.map <- ggplot(data = world) +
     geom_sf() +
-    annotation_scale(location = bar.position, width_hint = 0.5) +
+    annotation_scale(location = "tl", width_hint = 0.5) +
     annotation_north_arrow(location = "br", which_north = "true", 
                            pad_x = unit(0.1, "in"), pad_y = unit(0.1, "in"),
                            style = north_arrow_fancy_orienteering) +
@@ -40,7 +51,7 @@ make_map <- function(shape.file, file.name,scale.factor, bar.position) {
     #          color = "darkgrey", check_overlap = FALSE) +
     coord_sf(xlim = c(min.long, max.long), ylim = c(min.lat, max.lat), expand = FALSE)+
     geom_path(data = model.shape.df, aes(x = long, y = lat, group=group),
-              colour = "gray45", size=0.5) +
+              colour = "darkgrey") +
     xlab("Lon")+
     ylab("Lat")+
     theme_bw()
@@ -48,7 +59,8 @@ make_map <- function(shape.file, file.name,scale.factor, bar.position) {
   
   ggsave(file.name, model.map, scale = 2, dpi = 400)
   
-  return(model.map)
+  
   
 }
 
+make_map(shape.file="GOM_LL.shp", file.name = "gom_model_map.png")
